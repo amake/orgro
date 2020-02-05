add85377475b4e5217a70e4df03f99d4327b6959
Aaron Madlon-Kay <aaron@madlon-kay.com>
2020-02-05 23:38:11 +0900

    Implement file opening, passing to Flutter for iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,31 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+
+    private var openFileChannel : FlutterMethodChannel?
+
     override func application(
       _ application: UIApplication,
       didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         GeneratedPluginRegistrant.register(with: self)
+
+        if let viewController = window.rootViewController as? FlutterViewController {
+            openFileChannel = FlutterMethodChannel(name: "org.madlonkay.orgro/openFile", binaryMessenger: viewController.binaryMessenger)
+        }
+
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        guard let openFileChannel = openFileChannel else {
+            return false
+        }
+        if let content = try? String(contentsOf: url) {
+            openFileChannel.invokeMethod("loadString", arguments: content)
+            return true
+        } else {
+            return false
+        }
     }
 }
