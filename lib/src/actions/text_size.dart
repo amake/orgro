dcf363edd3d2633c1f2fdbdd78e2a458c38212a4
Aaron Madlon-Kay <aaron@madlon-kay.com>
2020-03-28 22:59:59 +0900

    Fix elevation shadow on text size popup
    Wrapping Material in {Size,Fade}Transition widgets killed the shadow. Refactored
    to put Material on top.

--- lib/src/actions/text_size.dart
+++ lib/src/actions/text_size.dart
@@ -44,16 +44,11 @@ class _TextSizeButtonState extends State<TextSizeButton>
       return Positioned(
         top: bounds.top,
         right: screen.width - bounds.right,
-        child: SizeTransition(
-          sizeFactor: animationController,
-          axis: Axis.horizontal,
-          axisAlignment: 1,
-          child: FadeTransition(
-            opacity: animationController,
-            child: TextSizePalette(
-              value: widget.value,
-              onChanged: widget.onChanged,
-            ),
+        child: PopupPalette(
+          animation: animationController,
+          child: TextSizeAdjuster(
+            value: widget.value,
+            onChanged: widget.onChanged,
           ),
         ),
       );
@@ -75,8 +70,37 @@ class _TextSizeButtonState extends State<TextSizeButton>
   }
 }
 
-class TextSizePalette extends StatefulWidget {
-  const TextSizePalette({
+class PopupPalette extends AnimatedWidget {
+  const PopupPalette({
+    @required Animation<double> animation,
+    @required this.child,
+    Key key,
+  })  : assert(child != null),
+        super(key: key, listenable: animation);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+    return Material(
+      type: MaterialType.card,
+      elevation: PopupMenuTheme.of(context)?.elevation ?? 8,
+      child: SizeTransition(
+        sizeFactor: animation,
+        axis: Axis.horizontal,
+        axisAlignment: 1,
+        child: FadeTransition(
+          opacity: animation,
+          child: child,
+        ),
+      ),
+    );
+  }
+}
+
+class TextSizeAdjuster extends StatefulWidget {
+  const TextSizeAdjuster({
     @required this.value,
     @required this.onChanged,
     Key key,
@@ -88,12 +112,12 @@ class TextSizePalette extends StatefulWidget {
   final Function(double) onChanged;
 
   @override
-  _TextSizePaletteState createState() => _TextSizePaletteState();
+  _TextSizeAdjusterState createState() => _TextSizeAdjusterState();
 }
 
 const _kTextSizeAdjustmentIncrement = 0.12;
 
-class _TextSizePaletteState extends State<TextSizePalette> {
+class _TextSizeAdjusterState extends State<TextSizeAdjuster> {
   double _value;
 
   @override
@@ -111,32 +135,22 @@ class _TextSizePaletteState extends State<TextSizePalette> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      type: MaterialType.card,
-      // TODO(aaron): Figure out why this still doesn't produce an elevation
-      // shadow
-      elevation: PopupMenuTheme.of(context).elevation ?? 8,
-      child: InkWell(
-        child: Row(
-          children: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.remove),
-              onPressed: () =>
-                  _setValue(_value - _kTextSizeAdjustmentIncrement),
-            ),
-            Text(
-              _value.toStringAsFixed(2),
-              style:
-                  const TextStyle(fontFeatures: [FontFeature.tabularFigures()]),
-            ),
-            IconButton(
-              icon: const Icon(Icons.add),
-              onPressed: () =>
-                  _setValue(_value + _kTextSizeAdjustmentIncrement),
-            )
-          ],
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.remove),
+          onPressed: () => _setValue(_value - _kTextSizeAdjustmentIncrement),
         ),
-      ),
+        Text(
+          _value.toStringAsFixed(2),
+          style: const TextStyle(fontFeatures: [FontFeature.tabularFigures()]),
+        ),
+        IconButton(
+          icon: const Icon(Icons.add),
+          onPressed: () => _setValue(_value + _kTextSizeAdjustmentIncrement),
+        )
+      ],
     );
   }
 }
