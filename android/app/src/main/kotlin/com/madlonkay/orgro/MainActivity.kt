53fc243ffb67df105b894c8107f5cbda5bfd9802
Aaron Madlon-Kay <aaron@madlon-kay.com>
2020-05-29 23:34:20 +0900

    Remove native file receiving implementation in favor of plugin

--- android/app/src/main/kotlin/com/madlonkay/orgro/MainActivity.kt
+++ android/app/src/main/kotlin/com/madlonkay/orgro/MainActivity.kt
@@ -1,103 +1,12 @@
 package com.madlonkay.orgro
 
-import android.content.Intent
-import android.net.Uri
-import android.os.Bundle
-import android.util.Log
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
-import kotlinx.coroutines.*
-import java.io.InputStream
-import java.util.*
-import java.util.concurrent.atomic.AtomicBoolean
 
-class MainActivity : FlutterActivity(), CoroutineScope by MainScope() {
-
-    lateinit var channel: MethodChannel
-
-    private val loadQueue: Deque<Uri> = ArrayDeque()
-    private val ready = AtomicBoolean(false)
-
+class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
-
-        channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "org.madlonkay.orgro/openFile")
-        channel.setMethodCallHandler(this::handleMethod)
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        when (intent?.action) {
-            Intent.ACTION_VIEW -> {
-                if (!intent.consumed()) {
-                    intent.data?.let { uri ->
-                        if (ready.get()) {
-                            launch { loadUri(uri) }
-                        } else {
-                            synchronized(loadQueue) {
-                                loadQueue.push(uri)
-                            }
-                        }
-                        intent.consume()
-                    }
-                }
-            }
-        }
-    }
-
-    private fun handleMethod(call: MethodCall, result: MethodChannel.Result) {
-        when (call.method) {
-            "ready" -> {
-                ready.set(true)
-                synchronized(loadQueue) {
-                    while (loadQueue.isNotEmpty()) {
-                        val queued = loadQueue.pop()
-                        launch { loadUri(queued) }
-                    }
-                }
-                result.success(true)
-            }
-            else -> result.notImplemented()
-        }
-    }
-
-    private suspend fun loadUri(uri: Uri) = withContext(Dispatchers.IO) {
-        val start = System.currentTimeMillis()
-        contentResolver.openInputStream(uri)?.readText()?.let {
-            val end = System.currentTimeMillis()
-            Log.d("Orgro", "loading URI complete (${end - start} ms)")
-            withContext(Dispatchers.Main) {
-                channel.invokeMethod("loadString", it, InvocationLogger("loadString"))
-            }
-        }
     }
 }
-
-private class InvocationLogger(val method: String) : MethodChannel.Result {
-    override fun notImplemented() {
-        Log.e("Orgro", "$method not implemented")
-    }
-
-    override fun error(errorCode: String?, errorMessage: String?, errorDetails: Any?) {
-        Log.e("Orgro", "$method error; code=$errorCode, message=$errorMessage, details=$errorDetails")
-    }
-
-    override fun success(result: Any?) {
-        Log.d("Orgro", "$method success; result=$result")
-    }
-}
-
-fun InputStream.readText(): String = bufferedReader().use { it.readText() }
-
-private const val consumedFlag = "orgro-consumed"
-
-fun Intent.consume() {
-    putExtra(consumedFlag, true)
-}
-
-fun Intent.consumed(): Boolean = hasExtra(consumedFlag)
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,68 +3,11 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-
-    private var openFileChannel : FlutterMethodChannel!
-
     override func application(
       _ application: UIApplication,
       didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         GeneratedPluginRegistrant.register(with: self)
-
-        if let viewController = window.rootViewController as? FlutterViewController {
-            openFileChannel = FlutterMethodChannel(name: "org.madlonkay.orgro/openFile", binaryMessenger: viewController.binaryMessenger)
-            openFileChannel.setMethodCallHandler(handleMethod(call:result:))
-        }
-
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
-
-    func handleMethod(call: FlutterMethodCall, result: @escaping FlutterResult) {
-        switch (call.method) {
-        case "ready":
-            result(true)
-        default:
-            result(FlutterMethodNotImplemented)
-        }
-    }
-
-    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        switch options[.openInPlace] as? Bool {
-        case .some(true):
-            loadInPlace(url)
-        default:
-            loadCopiedFile(url)
-        }
-        return true
-    }
-
-    private func loadInPlace(_ url: URL) {
-        DispatchQueue.global().async {
-            // Adapted from https://github.com/palmin/open-in-place/blob/97d3e0cd9bb6f4e0a84e167b31d5dbf729c7aa8a/OpenInPlace/UrlCoordination.swift
-            let error: NSErrorPointer = nil
-            NSFileCoordinator(filePresenter: nil).coordinate(readingItemAt: url, options: [], error: error) { url in
-                if url.startAccessingSecurityScopedResource() {
-                    self.openFileChannel.invokeMethod("loadUrl", arguments: url.absoluteString) { result in
-                        url.stopAccessingSecurityScopedResource()
-                    }
-                }
-            }
-            // TODO: Error handling
-            if let error = error?.pointee {
-                print(error)
-            }
-        }
-    }
-
-    private func loadCopiedFile(_ url: URL) {
-        openFileChannel.invokeMethod("loadUrl", arguments: url.absoluteString) { result in
-            do {
-                try FileManager.default.removeItem(at: url)
-            } catch {
-                // TODO: Error handling
-                print(error)
-            }
-        }
     }
 }
