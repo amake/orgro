da2c5a21e4218640fddf3168f92a696a46cab15e
Aaron Madlon-Kay <aaron@madlon-kay.com>
2021-05-12 21:47:18 +0900

    Add About dialog

--- lib/src/pages/start.dart
+++ lib/src/pages/start.dart
@@ -57,16 +57,12 @@ class _StartPageState extends State<StartPage>
             value: () => _openOrgroManual(context),
             child: const Text('Orgro Manual'),
           ),
-          const PopupMenuDivider(),
-          const PopupMenuItem<VoidCallback>(
-            value: _visitSupportLink,
-            child: Text('Support · Feedback'),
-          ),
-          PopupMenuItem<VoidCallback>(
-            value: () => _openLicensePage(context),
-            child: const Text('Licenses'),
-          ),
-        ]
+        ],
+        const PopupMenuDivider(),
+        PopupMenuItem<VoidCallback>(
+          value: () => _openAboutDialog(context),
+          child: const Text('About'),
+        ),
       ],
     );
   }
@@ -372,6 +368,68 @@ class _SupportLink extends StatelessWidget {
       style: TextButton.styleFrom(primary: Theme.of(context).disabledColor),
     );
   }
+}
+
+class _AppIcon extends StatelessWidget {
+  const _AppIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: const BorderRadius.all(Radius.circular(8)),
+      child: Image.asset(
+        'assets/manual/orgro-icon.png',
+        scale: MediaQuery.of(context).devicePixelRatio,
+      ),
+    );
+  }
+}
+
+class _AboutItem extends StatelessWidget {
+  const _AboutItem({
+    required this.label,
+    required this.onPressed,
+    Key? key,
+  }) : super(key: key);
+
+  final String label;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton.icon(
+      onPressed: onPressed,
+      label: Text(label.toUpperCase()),
+      icon: const Icon(Icons.open_in_new),
+    );
+  }
+}
+
+void _openAboutDialog(BuildContext context) {
+  showAboutDialog(
+    context: context,
+    applicationName: 'Orgro',
+    applicationVersion: _version,
+    applicationIcon: const Padding(
+      padding: EdgeInsets.all(8),
+      child: _AppIcon(),
+    ),
+    children: [
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          _AboutItem(
+            label: 'Support · Feedback',
+            onPressed: _visitSupportLink,
+          ),
+          _AboutItem(
+            label: 'Changelog',
+            onPressed: _visitChangelogLink,
+          ),
+        ],
+      ),
+    ],
+  );
 }
 
 void _visitSupportLink() => launch(
