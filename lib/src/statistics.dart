d08f9ee3796acf7c17c3def4cc00fa5d7519a268
Aaron Madlon-Kay <aaron@madlon-kay.com>
2024-11-04 14:25:31 +0900

    Fix bugs with stats calculations

--- lib/src/statistics.dart
+++ lib/src/statistics.dart
@@ -1,82 +1,5 @@
 import 'package:org_flutter/org_flutter.dart';
 
-OrgTree recalculateListStats(OrgTree root, OrgListItem target) {
-  final (node: _, :path) = root.find((node) => identical(node, target))!;
-  final tree = path.reversed.whereType<OrgTree>().first;
-  final replacement = _recalculateListStats(tree);
-  return root.edit().find(tree)!.replace(replacement).commit() as OrgTree;
-}
-
-OrgTree _recalculateListStats(OrgTree tree) {
-  final stack = <_ProgressScope>[];
-  final finished = <_ProgressScope>[];
-
-  // This visitor encodes the logic that cookies only count immediate children,
-  // not all descendants. If we wanted to support
-  // `org-checkbox-hierarchical-statistics` we would adjust here.
-  void visit(OrgNode node) {
-    // Current scope stuff
-    if (node is OrgListItem) {
-      if (node.checkbox == '[X]') stack.last.done++;
-      if (node.checkbox != null) stack.last.total++;
-    } else if (node is OrgStatisticsFractionCookie ||
-        node is OrgStatisticsPercentageCookie) {
-      stack.last.cookies.add(node);
-    }
-
-    var pushedScope = false;
-    if (node is OrgTree || node is OrgListItem) {
-      stack.add(_ProgressScope(node));
-      pushedScope = true;
-    }
-    final children = node.children;
-    if (children != null) {
-      for (final child in children) {
-        visit(child);
-      }
-    }
-    if (pushedScope) finished.add(stack.removeLast());
-  }
-
-  visit(tree);
-
-  var result = tree;
-  var recalculate = false;
-  for (final scope in finished) {
-    final parent = scope.parent;
-    final skip = parent is OrgSection &&
-        parent.getProperties(':COOKIE_DATA:').firstOrNull == 'todo';
-    if (!skip) {
-      for (final cookie in scope.cookies) {
-        final newCookie = scope.updatedCookie(cookie);
-        result =
-            result.editNode(cookie)!.replace(newCookie).commit() as OrgTree;
-      }
-    }
-    if (parent is OrgListItem && parent.checkbox != null && scope.total > 0) {
-      final newCheckbox = scope.done == 0
-          ? '[ ]'
-          : scope.done == scope.total
-              ? '[X]'
-              : '[-]';
-      // The parent progress may have changed now, so we take a second pass
-      //
-      // TODO(aaron): See if there's a simple way to do this in a single pass
-      recalculate |= parent.checkbox != newCheckbox;
-      final newListItem = switch (parent) {
-        OrgListOrderedItem() => parent.copyWith(checkbox: newCheckbox),
-        OrgListUnorderedItem() => parent.copyWith(checkbox: newCheckbox),
-      };
-      result =
-          result.editNode(parent)!.replace(newListItem).commit() as OrgTree;
-    }
-  }
-  if (recalculate) {
-    result = _recalculateListStats(result);
-  }
-  return result;
-}
-
 class _ProgressScope {
   _ProgressScope(this.parent);
 
@@ -85,18 +8,119 @@ class _ProgressScope {
   int done = 0;
   final List<OrgNode> cookies = [];
 
-  OrgNode updatedCookie(OrgNode cookie) {
-    if (cookie is OrgStatisticsFractionCookie) {
-      return cookie.copyWith(
-          numerator: done.toString(), denominator: total.toString());
-    } else if (cookie is OrgStatisticsPercentageCookie) {
-      return cookie.copyWith(
-          percentage:
-              done == 0 ? '0' : (done / total * 100).round().toString());
+  OrgNode updatedCookie(OrgNode cookie) => switch (cookie) {
+        OrgStatisticsFractionCookie() => cookie.copyWith(
+            numerator: done.toString(), denominator: total.toString()),
+        OrgStatisticsPercentageCookie() => cookie.copyWith(
+            percentage:
+                done == 0 ? '0' : (done / total * 100).round().toString()),
+        _ => throw Error(),
+      };
+}
+
+OrgTree recalculateListStats(OrgTree root, OrgListItem target) {
+  final (node: _, :path) = root.find((node) => identical(node, target))!;
+  final tree = path.reversed.whereType<OrgTree>().first;
+  var replacement = _recalculateCheckboxes(tree);
+  replacement = _recalculateListStats(replacement);
+  return root.edit().find(tree)!.replace(replacement).commit() as OrgTree;
+}
+
+OrgTree _recalculateCheckboxes(OrgTree tree) {
+  final scopes = _visitListScopes(tree);
+
+  var result = tree;
+  var recalculate = false;
+  for (final scope in scopes) {
+    if (scope.total == 0) continue;
+
+    final parent = scope.parent;
+    if (parent is! OrgListItem || parent.checkbox == null) continue;
+
+    final newCheckbox = scope.done == 0
+        ? '[ ]'
+        : scope.done == scope.total
+            ? '[X]'
+            : '[-]';
+
+    if (parent.checkbox == newCheckbox) continue;
+
+    // The parent progress may have changed now, so we take another pass
+    //
+    // TODO(aaron): See if there's a simple way to do this in a single pass
+    recalculate = true;
+    final zipper = result.editNode(parent);
+    if (zipper != null) {
+      final newListItem = switch (parent) {
+        OrgListOrderedItem() => parent.copyWith(checkbox: newCheckbox),
+        OrgListUnorderedItem() => parent.copyWith(checkbox: newCheckbox),
+      };
+      result = zipper.replace(newListItem).commit() as OrgTree;
     } else {
-      throw Error();
+      // The parent has been removed from the tree by a previous edit; we
+      // are recalculating anyway so just ignore.
     }
   }
+  if (recalculate) {
+    result = _recalculateCheckboxes(result);
+  }
+  return result;
+}
+
+OrgTree _recalculateListStats(OrgTree tree) {
+  final scopes = _visitListScopes(tree);
+
+  var result = tree;
+  for (final scope in scopes) {
+    final parent = scope.parent;
+    if (parent is OrgSection &&
+        parent.getProperties(':COOKIE_DATA:').firstOrNull == 'todo') continue;
+    for (final cookie in scope.cookies) {
+      final newCookie = scope.updatedCookie(cookie);
+      result = result.editNode(cookie)!.replace(newCookie).commit() as OrgTree;
+    }
+  }
+  return result;
+}
+
+// This visitor encodes the logic that cookies only count immediate children,
+// not all descendants. If we wanted to support
+// `org-checkbox-hierarchical-statistics` we would adjust here.
+List<_ProgressScope> _visitListScopes(
+  OrgNode node, [
+  List<_ProgressScope>? stack,
+  List<_ProgressScope>? acc,
+]) {
+  stack ??= [];
+  acc ??= [];
+
+  // Current scope stuff
+  if (node is OrgListItem) {
+    if (node.checkbox == '[X]') stack.last.done++;
+    if (node.checkbox != null) stack.last.total++;
+  } else if (node is OrgStatisticsFractionCookie ||
+      node is OrgStatisticsPercentageCookie) {
+    stack.last.cookies.add(node);
+  }
+
+  if (stack.isNotEmpty && node is OrgSection) {
+    // Don't recurse any deeper than first tree
+    return acc;
+  }
+
+  var pushedScope = false;
+  if (node is OrgTree || node is OrgListItem) {
+    stack.add(_ProgressScope(node));
+    pushedScope = true;
+  }
+  final children = node.children;
+  if (children != null) {
+    for (final child in children) {
+      _visitListScopes(child, stack, acc);
+    }
+  }
+  if (pushedScope) acc.add(stack.removeLast());
+  return acc;
 }
 
 OrgTree recalculateHeadlineStats(OrgTree root, OrgHeadline target) {
@@ -111,46 +135,52 @@ OrgTree recalculateHeadlineStats(OrgTree root, OrgHeadline target) {
 }
 
 OrgTree _recalculateHeadlineStats(OrgTree tree) {
-  final stack = <_ProgressScope>[];
-  final finished = <_ProgressScope>[];
-
-  void visit(OrgTree tree) {
-    var pushedScope = false;
-    if (tree is OrgSection) {
-      pushedScope = true;
-      if (stack.isNotEmpty && tree.headline.keyword != null) {
-        if (tree.headline.keyword!.done) stack.last.done++;
-        stack.last.total++;
-      }
-
-      stack.add(_ProgressScope(tree));
-
-      tree.headline.visit<OrgNode>((node) {
-        if (node is OrgStatisticsFractionCookie ||
-            node is OrgStatisticsPercentageCookie) {
-          stack.last.cookies.add(node);
-        }
-        return true;
-      });
-    }
-    for (final child in tree.sections) {
-      visit(child);
-    }
-    if (pushedScope) finished.add(stack.removeLast());
-  }
-
-  visit(tree);
+  final scopes = _visitHeadlineScopes(tree);
 
   var result = tree;
-  for (final scope in finished) {
+  for (final scope in scopes) {
     final parent = scope.parent;
-    final skip = parent is OrgSection &&
-        parent.getProperties(':COOKIE_DATA:').firstOrNull == 'checkbox';
-    if (skip) continue;
+    if (parent is OrgSection &&
+        parent.getProperties(':COOKIE_DATA:').firstOrNull == 'checkbox') {
+      continue;
+    }
     for (final cookie in scope.cookies) {
       final newCookie = scope.updatedCookie(cookie);
       result = result.editNode(cookie)!.replace(newCookie).commit() as OrgTree;
     }
   }
   return result;
+}
+
+List<_ProgressScope> _visitHeadlineScopes(
+  OrgTree tree, [
+  List<_ProgressScope>? stack,
+  List<_ProgressScope>? acc,
+]) {
+  stack ??= [];
+  acc ??= [];
+  var pushedScope = false;
+  if (tree is OrgSection) {
+    pushedScope = true;
+    if (stack.isNotEmpty && tree.headline.keyword != null) {
+      if (tree.headline.keyword!.done) stack.last.done++;
+      stack.last.total++;
+    }
+
+    final scope = _ProgressScope(tree);
+    stack.add(scope);
+
+    tree.headline.visit<OrgNode>((node) {
+      if (node is OrgStatisticsFractionCookie ||
+          node is OrgStatisticsPercentageCookie) {
+        scope.cookies.add(node);
+      }
+      return true;
+    });
+  }
+  for (final child in tree.sections) {
+    _visitHeadlineScopes(child, stack);
+  }
+  if (pushedScope) acc.add(stack.removeLast());
+  return acc;
 }
---
+++ test/statistics_test.dart
@@ -0,0 +1,286 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:org_flutter/org_flutter.dart';
+import 'package:orgro/src/statistics.dart';
+
+void main() {
+  group('Statistics', () {
+    group('Lists', () {
+      test('Normalize empty', () {
+        final doc = OrgDocument.parse('''
+[%] and [/]
+- foo
+''');
+        final (path: _, node: target) = doc.find<OrgListItem>((_) => true)!;
+        final result = recalculateListStats(doc, target);
+        expect(result.toMarkup(), '''
+[0%] and [0/0]
+- foo
+''');
+      });
+      test('No action', () {
+        final doc = OrgDocument.parse('''
+[0%] and [0/1]
+- [ ] foo
+''');
+        final (path: _, node: target) = doc.find<OrgListItem>((_) => true)!;
+        final result = recalculateListStats(doc, target);
+        expect(result.toMarkup(), '''
+[0%] and [0/1]
+- [ ] foo
+''');
+      });
+      test('Complete', () {
+        final doc = OrgDocument.parse('''
+[0%] and [0/1]
+- [X] foo
+''');
+        final (path: _, node: target) = doc.find<OrgListItem>((_) => true)!;
+        final result = recalculateListStats(doc, target);
+        expect(result.toMarkup(), '''
+[100%] and [1/1]
+- [X] foo
+''');
+      });
+      test('Incomplete', () {
+        final doc = OrgDocument.parse('''
+[100%] and [1/1]
+- [ ] foo
+''');
+        final (path: _, node: target) = doc.find<OrgListItem>((_) => true)!;
+        final result = recalculateListStats(doc, target);
+        expect(result.toMarkup(), '''
+[0%] and [0/1]
+- [ ] foo
+''');
+      });
+      test('Partial', () {
+        final doc = OrgDocument.parse('''
+[0%] and [0/1]
+- [ ] foo [0%] and [0/2]
+  - [X] bar
+  - [ ] baz
+''');
+        final (path: _, node: target) =
+            doc.find<OrgListItem>((item) => item.body!.toMarkup() == 'bar\n')!;
+        final result = recalculateListStats(doc, target);
+        expect(result.toMarkup(), '''
+[0%] and [0/1]
+- [-] foo [50%] and [1/2]
+  - [X] bar
+  - [ ] baz
+''');
+      });
+      test('Deep partial', () {
+        final doc = OrgDocument.parse('''
+[0%] and [0/1]
+- [ ] foo [0%] and [0/2]
+  - [X] bar
+  - [ ] baz
+    - [X] bazinga
+    - [ ] bazoonga
+''');
+        final (path: _, node: target) =
+            doc.find<OrgListItem>((item) => item.body!.toMarkup() == 'bar\n')!;
+        final result = recalculateListStats(doc, target);
+        expect(result.toMarkup(), '''
+[0%] and [0/1]
+- [-] foo [50%] and [1/2]
+  - [X] bar
+  - [-] baz
+    - [X] bazinga
+    - [ ] bazoonga
+''');
+      });
+      test('Cascade to complete', () {
+        final doc = OrgDocument.parse('''
+[0%] and [0/1]
+- [-] foo [0%] and [0/2]
+  - [X] bar
+  - [-] baz
+    - [X] bazinga
+    - [X] bazoonga
+''');
+        final (path: _, node: target) = doc.find<OrgListItem>(
+            (item) => item.body!.toMarkup() == 'bazoonga\n')!;
+        final result = recalculateListStats(doc, target);
+        expect(result.toMarkup(), '''
+[100%] and [1/1]
+- [X] foo [100%] and [2/2]
+  - [X] bar
+  - [X] baz
+    - [X] bazinga
+    - [X] bazoonga
+''');
+      });
+      test('Cascade to incomplete', () {
+        final doc = OrgDocument.parse('''
+[100%] and [1/1]
+- [X] foo [100%] and [2/2]
+  - [X] bar
+  - [X] baz
+    - [X] bazinga
+    - [ ] bazoonga
+''');
+        final (path: _, node: target) = doc.find<OrgListItem>(
+            (item) => item.body!.toMarkup() == 'bazoonga\n')!;
+        final result = recalculateListStats(doc, target);
+        expect(result.toMarkup(), '''
+[0%] and [0/1]
+- [-] foo [50%] and [1/2]
+  - [X] bar
+  - [-] baz
+    - [X] bazinga
+    - [ ] bazoonga
+''');
+      });
+      test('In headline', () {
+        final doc = OrgDocument.parse('''
+* headline [%] and [/]
+- [X] foo
+''');
+        final (path: _, node: target) = doc.find<OrgListItem>((_) => true)!;
+        final result = recalculateListStats(doc, target);
+        expect(result.toMarkup(), '''
+* headline [100%] and [1/1]
+- [X] foo
+''');
+      });
+      test('In headline but ignored', () {
+        final doc = OrgDocument.parse('''
+* headline [%] and [/]
+  :PROPERTIES:
+  :COOKIE_DATA: todo
+  :END:
+- [X] foo
+''');
+        final (path: _, node: target) = doc.find<OrgListItem>((_) => true)!;
+        final result = recalculateListStats(doc, target);
+        expect(result.toMarkup(), '''
+* headline [%] and [/]
+  :PROPERTIES:
+  :COOKIE_DATA: todo
+  :END:
+- [X] foo
+''');
+      });
+      test('Root scope', () {
+        final doc = OrgDocument.parse('''
+Root [%] and [/]
+- [X] foo
+* headline [%] and [/]
+** subheadline [%] and [/]
+''');
+        final (path: _, node: target) = doc.find<OrgListItem>((_) => true)!;
+        final result = recalculateListStats(doc, target);
+        expect(result.toMarkup(), '''
+Root [100%] and [1/1]
+- [X] foo
+* headline [%] and [/]
+** subheadline [%] and [/]
+''');
+      });
+      test('Section scope', () {
+        final doc = OrgDocument.parse('''
+Root [%] and [/]
+* headline [%] and [/]
+- [X] foo
+** subheadline [%] and [/]
+''');
+        final (path: _, node: target) = doc.find<OrgListItem>((_) => true)!;
+        final result = recalculateListStats(doc, target);
+        expect(result.toMarkup(), '''
+Root [%] and [/]
+* headline [100%] and [1/1]
+- [X] foo
+** subheadline [%] and [/]
+''');
+      });
+    });
+    group('Headlines', () {
+      test('Normalize empty', () {
+        final doc = OrgDocument.parse('''
+* top [%] and [/]
+** foo
+''');
+        final (path: _, node: target) =
+            doc.find<OrgHeadline>((headline) => headline.rawTitle == 'foo')!;
+        final result = recalculateHeadlineStats(doc, target);
+        expect(result.toMarkup(), '''
+* top [0%] and [0/0]
+** foo
+''');
+      });
+      test('No action', () {
+        final doc = OrgDocument.parse('''
+* top [0%] and [0/1]
+** TODO foo
+''');
+        final (path: _, node: target) =
+            doc.find<OrgHeadline>((headline) => headline.rawTitle == 'foo')!;
+        final result = recalculateHeadlineStats(doc, target);
+        expect(result.toMarkup(), '''
+* top [0%] and [0/1]
+** TODO foo
+''');
+      });
+      test('Complete', () {
+        final doc = OrgDocument.parse('''
+* top [0%] and [0/1]
+** DONE foo
+''');
+        final (path: _, node: target) =
+            doc.find<OrgHeadline>((headline) => headline.rawTitle == 'foo')!;
+        final result = recalculateHeadlineStats(doc, target);
+        expect(result.toMarkup(), '''
+* top [100%] and [1/1]
+** DONE foo
+''');
+      });
+      test('Incomplete', () {
+        final doc = OrgDocument.parse('''
+* top [100%] and [1/1]
+** TODO foo
+''');
+        final (path: _, node: target) =
+            doc.find<OrgHeadline>((headline) => headline.rawTitle == 'foo')!;
+        final result = recalculateHeadlineStats(doc, target);
+        expect(result.toMarkup(), '''
+* top [0%] and [0/1]
+** TODO foo
+''');
+      });
+      test('Remove keyword', () {
+        final doc = OrgDocument.parse('''
+* top [100%] and [1/1]
+** foo
+''');
+        final (path: _, node: target) =
+            doc.find<OrgHeadline>((headline) => headline.rawTitle == 'foo')!;
+        final result = recalculateHeadlineStats(doc, target);
+        expect(result.toMarkup(), '''
+* top [0%] and [0/0]
+** foo
+''');
+      });
+      test('Ignored', () {
+        final doc = OrgDocument.parse('''
+* top [%] and [/]
+  :PROPERTIES:
+  :COOKIE_DATA: checkbox
+  :END:
+** TODO foo
+''');
+        final (path: _, node: target) =
+            doc.find<OrgHeadline>((headline) => headline.rawTitle == 'foo')!;
+        final result = recalculateHeadlineStats(doc, target);
+        expect(result.toMarkup(), '''
+* top [%] and [/]
+  :PROPERTIES:
+  :COOKIE_DATA: checkbox
+  :END:
+** TODO foo
+''');
+      });
+    });
+  });
+}
