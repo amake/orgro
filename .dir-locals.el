78b0bf6af655a69abf6fbe1fd73b7e34bb32fffd
Aaron Madlon-Kay <aaron@madlon-kay.com>
2025-04-06 10:55:27 +0900

    Ignore /flutter in Emacs projectile

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((projectile-indexing-method . hybrid))))
