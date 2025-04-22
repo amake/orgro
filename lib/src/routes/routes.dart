55874825890fca0a0a5428633dfcee400f84be43
Aaron Madlon-Kay <aaron@madlon-kay.com>
2025-04-22 22:52:13 +0900

    Kick unrecognized URLs back to the browser

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -4,6 +4,7 @@ import 'package:orgro/src/data_source.dart';
 import 'package:orgro/src/routes/document.dart';
 import 'package:orgro/src/routes/narrow.dart';
 import 'package:orgro/src/routes/settings.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Routes {
   const Routes._();
@@ -41,5 +42,15 @@ Route<dynamic>? onGenerateRoute(RouteSettings settings) {
         }
       }
   }
+
+  // We shouldn't be receiving any unknown paths here, but if the deep link
+  // settings change out from under us then it's possible. In that case we
+  // should kick back to the browser.
+  final externalUri = Uri.https('orgro.org', uri.path);
+  launchUrl(externalUri, mode: LaunchMode.externalApplication).onError((e, s) {
+    debugPrint('Error launching "$externalUri": $e');
+    return false;
+  });
+
   return null;
 }
