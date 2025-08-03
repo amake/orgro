d281b76e454936f19f32e2610878a4ba306cbca6
Aaron Madlon-Kay <aaron@madlon-kay.com>
2025-08-03 09:21:42 +0900

    Make state explicit

--- lib/src/components/scroll.dart
+++ lib/src/components/scroll.dart
@@ -1,31 +1,48 @@
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
-class HideOnScroll extends StatelessWidget {
+class HideOnScroll extends StatefulWidget {
   const HideOnScroll({required this.child, super.key});
 
   final Widget child;
 
   @override
+  State<HideOnScroll> createState() => _HideOnScrollState();
+}
+
+class _HideOnScrollState extends State<HideOnScroll> {
+  late ScrollController _controller;
+  bool _showFab = true;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _controller = PrimaryScrollController.of(context);
+    _controller.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_onScroll);
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (_controller.hasClients &&
+        _controller.position.userScrollDirection != ScrollDirection.idle) {
+      setState(() {
+        _showFab =
+            _controller.position.userScrollDirection == ScrollDirection.forward;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    var showFab = true;
-    return AnimatedBuilder(
-      animation: PrimaryScrollController.of(context),
-      builder: (context, child) {
-        final controller = PrimaryScrollController.of(context);
-        if (controller.hasClients &&
-            controller.position.userScrollDirection != ScrollDirection.idle) {
-          showFab =
-              controller.position.userScrollDirection ==
-              ScrollDirection.forward;
-        }
-        return AnimatedScale(
-          duration: const Duration(milliseconds: 100),
-          scale: showFab ? 1 : 0,
-          child: child,
-        );
-      },
-      child: child,
+    return AnimatedScale(
+      duration: const Duration(milliseconds: 100),
+      scale: _showFab ? 1 : 0,
+      child: widget.child,
     );
   }
 }
