f563f6d67f323b0953c5410d0bce65ec04955981
Aaron Madlon-Kay <aaron@madlon-kay.com>
2025-08-30 17:07:14 +0900

    Revert "Adopt UISceneDelegate"
    This reverts commit 5317d18ac405adf926a96be4a789ea251e6dedcc.
    
    This breaks state restoration; see https://github.com/flutter/flutter/issues/174402

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,22 +2,17 @@ import UIKit
 import Flutter
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterPluginRegistrant {
+@objc class AppDelegate: FlutterAppDelegate {
     override func application(
       _ application: UIApplication,
       didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        pluginRegistrant = self
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
-
-    func register(with registry: any FlutterPluginRegistry) {
-        let registrar = registry.registrar(forPlugin: "native_search")!
-
-        let channel = FlutterMethodChannel(name: "com.madlonkay.orgro/native_search", binaryMessenger: registrar.messenger())
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let channel = FlutterMethodChannel(name: "com.madlonkay.orgro/native_search", binaryMessenger: controller.binaryMessenger)
 
         channel.setMethodCallHandler(handleNativeSearchMethod)
 
-        GeneratedPluginRegistrant.register(with: registry)
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 }
