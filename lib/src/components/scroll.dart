ab8db4d147716cae56c497e116625e4f3187daa5
Aaron Madlon-Kay <aaron@madlon-kay.com>
2025-10-14 09:49:41 +0900

    Fix occasional lock in scrolling state detection

--- lib/src/components/scroll.dart
+++ lib/src/components/scroll.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
@@ -12,6 +14,7 @@ class ScrollingBuilder extends StatefulWidget {
 
 class _ScrollingBuilderState extends State<ScrollingBuilder> {
   late final ScrollController _controller;
+  Timer? _timer;
   bool _isScrolling = false;
 
   @override
@@ -23,14 +26,22 @@ class _ScrollingBuilderState extends State<ScrollingBuilder> {
 
   void _onScroll() {
     if (!mounted) return;
+    if (_timer != null) return;
+
     // If we check the scroll direction immediately, we will never find the idle
-    // state. Thus we schedule the check for the next frame.
-    WidgetsBinding.instance.addPostFrameCallback((_) {
+    // state. Thus we *would* schedule the check for the next frame, but
+    // sometimes it seems that the next frame won't reliably find the idle state
+    // either. Thus we poll until we find the idle state.
+    _timer = Timer.periodic(const Duration(milliseconds: 100), (timer) {
       final isScrolling =
           _controller.hasClients &&
           _controller.position.userScrollDirection != ScrollDirection.idle;
       if (isScrolling != _isScrolling) {
         setState(() => _isScrolling = isScrolling);
+      }
+      if (!isScrolling) {
+        timer.cancel();
+        _timer = null;
       }
     });
   }
@@ -38,6 +49,7 @@ class _ScrollingBuilderState extends State<ScrollingBuilder> {
   @override
   void dispose() {
     _controller.removeListener(_onScroll);
+    _timer?.cancel();
     super.dispose();
   }
 
