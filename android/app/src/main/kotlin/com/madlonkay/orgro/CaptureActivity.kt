1cfaa86d83db0fafeb04b50709cf086130897314
Aaron Madlon-Kay <aaron@madlon-kay.com>
2025-11-26 09:59:03 +0900

    Implement Android capture activity

---
+++ android/app/src/main/kotlin/com/madlonkay/orgro/CaptureActivity.kt
@@ -0,0 +1,80 @@
+package com.madlonkay.orgro
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+
+class CaptureActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        when (intent?.action) {
+            Intent.ACTION_SEND -> {
+                when (intent.type) {
+                    "text/plain" -> handleSendText(intent)
+                    else -> {
+                        Log.d("OrgroCapture", "Unhandleable intent type: ${intent.type}")
+                    }
+                }
+            }
+            else -> {
+                Log.d("OrgroCapture", "Unhandleable intent action: ${intent?.action}")
+            }
+        }
+        finish()
+    }
+
+    private fun handleSendText(intent: Intent) {
+        var url: String? = null
+        var title: String? = null
+        var body: String? = null
+        intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+            Log.d("OrgroCapture", "Got text: $it")
+            if (isUri(it)) {
+                url = it
+            } else {
+                body = it
+            }
+        }
+        title = intent.getStringExtra(Intent.EXTRA_TITLE)?.let {
+            Log.d("OrgroCapture", "Got title: $it")
+            it
+        }
+        title = title ?: intent.getStringExtra(Intent.EXTRA_SUBJECT)?.let {
+            Log.d("OrgroCapture", "Got subject: $it")
+            it
+        }
+        shareParts(url, title, body)
+    }
+
+    private fun isUri(str: String): Boolean {
+        return try {
+            Uri.parse(str).scheme?.isNotBlank() == true
+        } catch (e: IllegalArgumentException) {
+            false
+        }
+    }
+
+    private val orgProtocolScheme
+        get() = getString(when (BuildConfig.BUILD_TYPE) {
+            "debug" -> R.string.org_protocol_scheme_debug
+            "profile" -> R.string.org_protocol_scheme_profile
+            "release" -> R.string.org_protocol_scheme
+            else -> throw Exception()
+        })
+
+    private fun shareParts(url: String?, title: String?, body: String?) {
+        val builder = Uri.Builder()
+            .scheme(orgProtocolScheme)
+            .authority("capture")
+        if (url != null) builder.appendQueryParameter("url", url)
+        if (title != null) builder.appendQueryParameter("title", title)
+        if (body != null) builder.appendQueryParameter("body", body)
+        val intent = Intent(this, MainActivity::class.java).apply {
+            data = builder.build()
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+        startActivity(intent)
+    }
+}
