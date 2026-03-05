953fba3dea54c58cab8da903812ad0df9d32a946
Aaron Madlon-Kay <aaron@madlon-kay.com>
2026-03-06 00:34:21 +0900

    Expand DocumentAnalysis unit tests

--- test/analysis_test.dart
+++ test/analysis_test.dart
@@ -11,6 +11,33 @@ void main() {
       expect(analysis.hasRelativeLinks, isFalse);
       expect(analysis.hasEncryptedContent, isFalse);
       expect(analysis.needsEncryption, isFalse);
+      expect(analysis.hasAgendaEntries, isFalse);
+    });
+    test('Exciting document', () {
+      final doc = OrgDocument.parse('''
+* [#A] foo
+* TODO bar :tag1:tag2:
+  SCHEDULED: <2024-01-01>
+** DONE [#C] baz :tag3:crypt:
+  https://example.com/image.png
+  [[./foo.org][foo]]
+
+-----BEGIN PGP MESSAGE-----
+
+jA0ECQMI05i+dd7lsRry0joBcGZQ4m+N9M/Z3I2Xw7SSn2uPMRpWbH9UIRkzPTXU
+28y0FO/Aug4JaE/n+wqbmgY0b37dZzWI2BWi
+=uuok
+-----END PGP MESSAGE-----
+''');
+      final analysis = DocumentAnalysis.of(doc, canResolveRelativeLinks: true);
+      expect(analysis.hasRemoteImages, isTrue);
+      expect(analysis.hasRelativeLinks, isTrue);
+      expect(analysis.hasEncryptedContent, isTrue);
+      expect(analysis.needsEncryption, isTrue);
+      expect(analysis.hasAgendaEntries, isTrue);
+      expect(analysis.keywords, ['TODO', 'DONE']);
+      expect(analysis.priorities, ['A', 'C']);
+      expect(analysis.tags, ['tag1', 'tag2', 'tag3', 'crypt']);
     });
     test('Remote images', () {
       final doc = OrgDocument.parse('https://example.com/image.png');
@@ -19,6 +46,7 @@ void main() {
       expect(analysis.hasRelativeLinks, isFalse);
       expect(analysis.hasEncryptedContent, isFalse);
       expect(analysis.needsEncryption, isFalse);
+      expect(analysis.hasAgendaEntries, isFalse);
     });
     test('Relative links', () {
       final doc = OrgDocument.parse('[[./foo.org][foo]]');
@@ -27,6 +55,7 @@ void main() {
       expect(analysis.hasRelativeLinks, isTrue);
       expect(analysis.hasEncryptedContent, isFalse);
       expect(analysis.needsEncryption, isFalse);
+      expect(analysis.hasAgendaEntries, isFalse);
     });
     test('Encrypted content', () {
       final doc = OrgDocument.parse('''
@@ -44,6 +73,7 @@ jA0ECQMI05i+dd7lsRry0joBcGZQ4m+N9M/Z3I2Xw7SSn2uPMRpWbH9UIRkzPTXU
       expect(analysis.hasRelativeLinks, isFalse);
       expect(analysis.hasEncryptedContent, isTrue);
       expect(analysis.needsEncryption, isFalse);
+      expect(analysis.hasAgendaEntries, isFalse);
     });
     test('Needs encryption', () {
       final doc = OrgDocument.parse('''
@@ -63,6 +93,7 @@ jA0ECQMI05i+dd7lsRry0joBcGZQ4m+N9M/Z3I2Xw7SSn2uPMRpWbH9UIRkzPTXU
       expect(analysis.hasRelativeLinks, isFalse);
       expect(analysis.hasEncryptedContent, isTrue);
       expect(analysis.needsEncryption, isTrue);
+      expect(analysis.hasAgendaEntries, isFalse);
     });
     test('Keywords', () {
       final doc = OrgDocument.parse('''
@@ -93,6 +124,18 @@ jA0ECQMI05i+dd7lsRry0joBcGZQ4m+N9M/Z3I2Xw7SSn2uPMRpWbH9UIRkzPTXU
       expect(analysis.keywords, isEmpty);
       expect(analysis.tags, isEmpty);
       expect(analysis.priorities, ['A', 'B']);
+    });
+    test('Agenda entries', () {
+      final doc = OrgDocument.parse('''
+* foo
+  SCHEDULED: <2024-01-01>
+''');
+      final analysis = DocumentAnalysis.of(doc, canResolveRelativeLinks: true);
+      expect(analysis.hasRemoteImages, isFalse);
+      expect(analysis.hasRelativeLinks, isFalse);
+      expect(analysis.hasEncryptedContent, isFalse);
+      expect(analysis.needsEncryption, isFalse);
+      expect(analysis.hasAgendaEntries, isTrue);
     });
   });
 }
